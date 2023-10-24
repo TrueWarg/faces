903806b4de482629e544e0a4f983995b47f31b44
TrueWarg <grazashar@mail.ru>
2023-10-24 23:19:08 +0200

    Add basic animation for character

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use bevy::{
-    prelude::{App, PluginGroup, Startup, Update},
+    prelude::{App, PluginGroup, Startup, Update, IntoSystemConfigs},
     window::{Window, WindowPlugin},
     DefaultPlugins,
 };
-use bevy_rapier2d::prelude::{RapierPhysicsPlugin, NoUserData};
+use bevy_rapier2d::prelude::{NoUserData, RapierPhysicsPlugin};
+use playground::CharacterAnimationResource;
+use ron::de::from_bytes;
 
-mod states;
-mod startup;
-mod resources;
 mod playground;
+mod resources;
+mod startup;
+mod states;
 
 fn main() {
     App::new()
+        .insert_resource(
+            from_bytes::<CharacterAnimationResource>(include_bytes!(
+                "../data/character_animations.ron"
+            ))
+            .unwrap(),
+        )
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
@@ -21,7 +29,16 @@ fn main() {
             ..Default::default()
         }))
         .add_state::<states::GameState>()
-        .add_systems(Startup, (playground::setup, playground::spawn_player))
+        .add_systems(
+            Startup,
+            (
+                playground::setup,
+                playground::spawn_player,
+                playground::spawn_nps,
+            ),
+        )
         .add_systems(Update, playground::player_movement)
+        .add_systems(Update, playground::set_player_animation_system.after(playground::player_movement))
+        .add_systems(Update, playground::animate_character_system.after(playground::set_player_animation_system))
         .run();
 }
