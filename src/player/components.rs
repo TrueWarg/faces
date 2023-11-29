e433560e8d4173747ca029516bb67bdc44808e89
TrueWarg <grazashar@mail.ru>
2023-11-30 00:46:20 +0200

    Add basic player fight animations

--- src/player/components.rs
+++ src/player/components.rs
@@ -1,14 +1,25 @@
-use bevy::{prelude::Component, time::Timer};
+use bevy::{prelude::Component, time::Timer, asset::Handle, sprite::TextureAtlas};
 
-use super::types::MoveDirection;
+use super::types::{FightDirection, MoveDirection};
 
 #[derive(Component, Debug)]
 pub struct Player {
     pub speed: f32,
+    pub is_fights: bool,
 }
 
 #[derive(Component)]
-pub struct MoveAnimationComponent {
+pub struct MoveAnimation {
     pub timer: Timer,
     pub direction: MoveDirection,
+    // todo: remove it, find more convential method to get particular sheets
+    pub sheet_handle: Handle<TextureAtlas>,
+}
+
+#[derive(Component)]
+pub struct FightAnimation {
+    pub timer: Timer,
+    pub direction: FightDirection,
+    // todo: remove it, find more convential method to get particular sheets
+    pub sheet_handle: Handle<TextureAtlas>,
 }
