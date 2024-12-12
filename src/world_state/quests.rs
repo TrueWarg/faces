98924da1dd61db267cc8e036d0c48f76d42c7687
TrueWarg <grazashar@mail.ru>
2024-12-12 20:44:31 +0200

    Add quests stages

--- src/world_state/quests.rs
+++ src/world_state/quests.rs
@@ -9,3 +9,56 @@ pub enum EscapeFromHouse {
     Escape,
     Completed,
 }
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+pub enum EnterTheCourt {
+    #[default]
+    None,
+    Go,
+    StopDrevnira,
+    Completed,
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+pub enum InCourHall {
+    #[default]
+    None,
+    TalkWithManager,
+    Wait,
+    Completed,
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+pub enum Court {
+    #[default]
+    None,
+    Talk,
+    Completed,
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+pub enum House {
+    #[default]
+    GoSleep,
+    TalkWithPolice,
+    Completed,
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+pub enum StrangeOldWoman {
+    #[default]
+    None,
+    GiveMeFeather,
+    Beaten,
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+pub enum Gopniks {
+    #[default]
+    None,
+    TalkWithBlond,
+    TalkWithGopniks,
+    GiveDumplingsToBlond,
+    TakeDumplingsFromBlond,
+    Completed,
+}
