e3be5e7df80873bbf1a998c857511a7787a64819
TrueWarg <grazashar@mail.ru>
2025-01-04 17:54:08 +0200

    Hall: setup fixed npc

--- src/level/courthouse_hall.rs
+++ src/level/courthouse_hall.rs
@@ -1,12 +1,14 @@
 use bevy::app::{Plugin, Update};
-use bevy::asset::AssetServer;
+use bevy::asset::{Assets, AssetServer};
 use bevy::math::Vec3;
-use bevy::prelude::{Commands, in_state, IntoSystemConfigs, OnEnter, OnExit, Res, SpriteBundle, States, Transform};
+use bevy::prelude::{Commands, in_state, IntoSystemConfigs, OnEnter, OnExit, Query, Res, ResMut, SpriteBundle, States, TextureAtlasLayout, Transform};
 use bevy_rapier2d::dynamics::RigidBody;
+use crate::animation::entities::MoveDirection;
 use crate::core::collisions::recalculate_z;
 use crate::core::entities::LevelYMax;
 use crate::core::z_index::{calculate_z, FLOOR_Z, ON_WALL_OBJECT_Z, WALL_Z};
 use crate::level::objects::spawn_object;
+use crate::npc::spawn_fixed_npc;
 
 pub struct CourtHouseHallPlugin<S: States> {
     pub state: S,
@@ -16,6 +18,11 @@ impl<S: States> Plugin for CourtHouseHallPlugin<S> {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.add_systems(OnEnter(self.state.clone()), load)
             .add_systems(OnExit(self.state.clone()), unload)
+            .add_systems(OnEnter(self.state.clone()), spawn_guardians)
+            .add_systems(OnEnter(self.state.clone()), spawn_working_clerks)
+            .add_systems(OnEnter(self.state.clone()), spawn_speaking_clerks)
+            .add_systems(OnEnter(self.state.clone()), spawn_visitors)
+            .add_systems(OnEnter(self.state.clone()), spawn_crazy_man)
             .add_systems(Update, (recalculate_z).run_if(in_state(self.state.clone())));
     }
 }
@@ -146,6 +153,247 @@ fn spawn_doors(
             },
             ..Default::default()
         });
+}
+
+fn spawn_guardians(
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    mut layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    spawn_fixed_npc(
+        &asset_server,
+        &mut commands,
+        &mut layouts,
+        (),
+        "npc/guardian.png".to_string(),
+        MoveDirection::ForwardIdle,
+        -50.0,
+        350.0,
+        ON_WALL_OBJECT_Z + 1.5,
+    );
+
+    spawn_fixed_npc(
+        &asset_server,
+        &mut commands,
+        &mut layouts,
+        (),
+        "npc/guardian.png".to_string(),
+        MoveDirection::ForwardIdle,
+        50.0,
+        350.0,
+        ON_WALL_OBJECT_Z + 1.5,
+    );
+}
+
+fn spawn_working_clerks(
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    mut layouts: ResMut<Assets<TextureAtlasLayout>>,
+    level_y_max: Query<&LevelYMax>,
+) {
+    let default = LevelYMax::create(359.0);
+    let y_max = level_y_max.get_single().unwrap_or(&default);
+
+    let x = 190.0;
+    let y = 200.0;
+    let shifted_y = y - 20.0;
+    let z = calculate_z(shifted_y, y_max.value);
+    spawn_fixed_npc(
+        &asset_server,
+        &mut commands,
+        &mut layouts,
+        (),
+        "npc/clerk.png".to_string(),
+        MoveDirection::LeftIdle,
+        x,
+        y,
+        z,
+    );
+
+    let y = 90.0;
+    let shifted_y = y - 20.0;
+    let z = calculate_z(shifted_y, y_max.value);
+    spawn_fixed_npc(
+        &asset_server,
+        &mut commands,
+        &mut layouts,
+        (),
+        "npc/clerk_blond.png".to_string(),
+        MoveDirection::LeftIdle,
+        x,
+        y,
+        z,
+    );
+
+    let y = -20.0;
+    let shifted_y = y - 20.0;
+    let z = calculate_z(shifted_y, y_max.value);
+    spawn_fixed_npc(
+        &asset_server,
+        &mut commands,
+        &mut layouts,
+        (),
+        "npc/clerk.png".to_string(),
+        MoveDirection::LeftIdle,
+        x,
+        y,
+        z,
+    );
+}
+
+fn spawn_speaking_clerks(
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    mut layouts: ResMut<Assets<TextureAtlasLayout>>,
+    level_y_max: Query<&LevelYMax>,
+) {
+    let default = LevelYMax::create(359.0);
+    let y_max = level_y_max.get_single().unwrap_or(&default);
+
+    let x = 160.0;
+    let y = -190.0;
+    let shifted_y = y - 20.0;
+    let z = calculate_z(shifted_y, y_max.value);
+    spawn_fixed_npc(
+        &asset_server,
+        &mut commands,
+        &mut layouts,
+        (),
+        "npc/clerk.png".to_string(),
+        MoveDirection::RightIdle,
+        x,
+        y,
+        z,
+    );
+
+    let x = 190.0;
+    spawn_fixed_npc(
+        &asset_server,
+        &mut commands,
+        &mut layouts,
+        (),
+        "npc/clerk_blond.png".to_string(),
+        MoveDirection::LeftIdle,
+        x,
+        y,
+        z,
+    );
+}
+
+fn spawn_visitors(
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    mut layouts: ResMut<Assets<TextureAtlasLayout>>,
+    level_y_max: Query<&LevelYMax>,
+) {
+    let default = LevelYMax::create(359.0);
+    let y_max = level_y_max.get_single().unwrap_or(&default);
+
+    let x = -120.0;
+    let y = 190.0;
+    let shifted_y = y - 20.0;
+    let z = calculate_z(shifted_y, y_max.value);
+    spawn_fixed_npc(
+        &asset_server,
+        &mut commands,
+        &mut layouts,
+        (),
+        "npc/woman_pink.png".to_string(),
+        MoveDirection::ForwardIdle,
+        x,
+        y,
+        z,
+    );
+
+    let x = -160.0;
+    let y = 150.0;
+    let shifted_y = y - 20.0;
+    let z = calculate_z(shifted_y, y_max.value);
+    spawn_fixed_npc(
+        &asset_server,
+        &mut commands,
+        &mut layouts,
+        (),
+        "npc/clerk.png".to_string(),
+        MoveDirection::RightIdle,
+        x,
+        y,
+        z,
+    );
+
+    let x = -120.0;
+    let y = 0.0;
+    let shifted_y = y - 20.0;
+    let z = calculate_z(shifted_y, y_max.value);
+    spawn_fixed_npc(
+        &asset_server,
+        &mut commands,
+        &mut layouts,
+        (),
+        "npc/clerk.png".to_string(),
+        MoveDirection::ForwardIdle,
+        x,
+        y,
+        z,
+    );
+
+    let x = -90.0;
+    let y = 10.0;
+    let shifted_y = y - 20.0;
+    let z = calculate_z(shifted_y, y_max.value);
+    spawn_fixed_npc(
+        &asset_server,
+        &mut commands,
+        &mut layouts,
+        (),
+        "npc/woman_green.png".to_string(),
+        MoveDirection::ForwardIdle,
+        x,
+        y,
+        z,
+    );
+
+    let x = -150.0;
+    let y = -150.0;
+    let shifted_y = y - 20.0;
+    let z = calculate_z(shifted_y, y_max.value);
+    spawn_fixed_npc(
+        &asset_server,
+        &mut commands,
+        &mut layouts,
+        (),
+        "npc/clerk.png".to_string(),
+        MoveDirection::RightIdle,
+        x,
+        y,
+        z,
+    );
+}
+
+fn spawn_crazy_man(
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    mut layouts: ResMut<Assets<TextureAtlasLayout>>,
+    level_y_max: Query<&LevelYMax>,
+) {
+    let default = LevelYMax::create(359.0);
+    let y_max = level_y_max.get_single().unwrap_or(&default);
+
+    let x = 170.0;
+    let y = 290.0;
+    let shifted_y = y - 20.0;
+    let z = calculate_z(shifted_y, y_max.value);
+    spawn_fixed_npc(
+        &asset_server,
+        &mut commands,
+        &mut layouts,
+        (),
+        "npc/crazy_man.png".to_string(),
+        MoveDirection::ForwardIdle,
+        x,
+        y,
+        z,
+    );
 }
 
 fn unload() {}
