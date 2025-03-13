403576b53d5321f8f597259200533b8335590285
TrueWarg <grazashar@mail.ru>
2025-03-13 20:36:23 +0200

    Add rename to character_screen

--- src/rpg/mod.rs
+++ src/rpg/mod.rs
@@ -3,7 +3,7 @@ pub use attacks::*;
 pub use interactions::*;
 pub use items::*;
 pub use stats::*;
-pub use level_up_ui::*;
+pub use character_screen::*;
 
 mod items;
 mod abilities;
@@ -11,7 +11,7 @@ mod stats;
 mod interactions;
 mod attacks;
 mod classes;
-mod level_up_ui;
+mod character_screen;
 mod characteristic_item_ui;
 mod stat_item_ui;
 
